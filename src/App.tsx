fb13182865bd47b29333f161c5848b9ae06046d9
unknown <rrrr0ffff@gmail.com>
2023-10-04 16:19:46 -0300

    stacked cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import comic01 from "./assets/comic01.jpg";
+import comic02 from "./assets/comic02.jpg";
+import comic03 from "./assets/comic03.jpg";
+
 function App() {
+	const comics = [comic01, comic02, comic03];
 	return (
-		<>
-			<div>Hello, World!</div>
-		</>
+		<div className="App">
+			<div className="carousel">
+				{comics.map((comic, index) => (
+					<img src={comic} key={index} className="card" />
+				))}
+			</div>
+		</div>
 	);
 }
 
